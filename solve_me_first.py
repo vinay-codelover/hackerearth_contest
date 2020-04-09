4539a709ec935f5ffd6a055a10ffad882d74dd10
vinay-techie <58105215+vinay-techie@users.noreply.github.com>
2020-04-09 10:18:15 +0530

    Update solve_me_first.py

--- solve_me_first.py
+++ solve_me_first.py
@@ -5,7 +5,7 @@
         temp = 1
         for i in range(0,len(nums)-1,2):
             if(nums[i+1]-nums[i]!=0):
-                temp =0 = 0
+                temp = 0
                 return(nums[i])
                 break
         if(temp==1):
